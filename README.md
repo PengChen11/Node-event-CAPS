95a97ef3e7c4428cd6e22c82d1ee020c22730cc4
Peng Chen <pchen0816@outlook.com>
2020-10-08 22:27:52 -0700

    working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Node-event-CAPS
+# Stand alone CAPS server for lab 18
+
+for more info, please visit https://github.com/PengChen11/node-event-server
---
+++ caps.js
@@ -0,0 +1,36 @@
+'use strict';
+require('dotenv').config();
+const io = require('socket.io')(3000);
+
+function eventHandler(eventName, room=null){
+  // The universal even handler
+  return payload=>{
+    const time = new Date();
+    console.log('EVENT', {event: eventName, time, payload});
+    if (!room) caps.emit(eventName, payload);
+    else {
+      caps.to(payload.store).emit(eventName, payload);
+    }
+  };
+}
+
+//Namespace of caps
+
+const caps = io.of('/caps');
+
+caps.on('connection', (socket)=>{
+  socket.on('join', (room)=>{
+    const valid = ['1-206-flowers', 'Lovers-flowers'];
+    if (valid.includes(room)){
+      console.log('Welcome, ', room);
+      socket.join(room);
+    }
+  });
+
+  socket.on('pickup', eventHandler('pickup'));
+
+  socket.on('in-transit', eventHandler('in-transit', true));
+
+  socket.on('delivered', eventHandler('delivered', true));
+});
+
